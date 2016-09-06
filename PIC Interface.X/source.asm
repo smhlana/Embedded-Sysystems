4afe8d9202f28bbda602bc6f9ce440b9106563cc
smhlana <smhlana98@gmail.com>
2016-09-06 07:27:09 +0200

    working on 1s delay

--- PIC Interface.X/source.asm
+++ PIC Interface.X/source.asm
@@ -5,6 +5,9 @@ W		EQU 00h
 FREQUENCY_L	EQU 20h
 FREQUENCY_H	EQU 21h
 T1SYNC		EQU 22h
+CNT_1		EQU 23h	
+CNT_2		EQU 24h
+CNT_3		EQU 25h
 ;*******************************************************************************
 
 ;**********BEGIN****************************************************************
@@ -24,7 +27,7 @@ Setup   bcf     STATUS,RP0
 
 ;**********CALCULATE FREQUENCY**************************************************
 CalculateFrequency
-	; INSERT 1S DELAY
+	call	One_S_Delay
 StartTimer1
 	bcf     STATUS,RP1	; Select Bank 0
 	bsf	T1CON, TMR1ON   ; Enable Timer 1
@@ -38,7 +41,34 @@ StopTTimer1
 	goto	CalculateVoltage
 ;*******************************************************************************
 
-;**********CALCULATE VOLTAGE**************************************************
+;**********CALCULATE VOLTAGE****************************************************
 CalculateVoltage
 ;*******************************************************************************
+	
+;**********1S DELAY*************************************************************
+;One_S_Delay
+;	movlw	00h	    ; Load 256
+;	movwf	CNT_1
+;	movlw	0DBh	    ; Load 219
+;	movwf	CNT_2
+;	movlw	06h	    ; Load 6
+;	movwf	CNT_3
+;Start_Delay
+;	decfsz	CNT_1
+;	goto	Start_Delay
+;	movlw	00h
+;	movwf	CNT_1
+;	decfsz	CNT_2
+;	movlw	0DBh
+;	movfw	CNT_2
+;	decfsz	CNT_3
+;	goto	Start_Delay
+;	return
+;*******************************************************************************
+One_S_Delay
+	movlw	00h
+Start_Delay
+	decfsz	CNT_1
+	goto	Start_Delay
+	return
 	end
